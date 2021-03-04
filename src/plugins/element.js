2c715fb9aef26526cc2218b79062e70b0a98bbaa
strangersj <strangersj@outlook.com>
2021-03-04 21:36:16 +0800

    安装测试 element-ui 依赖

---
+++ src/plugins/element.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+// 按需引入 element-ui 组件
+/* import {
+  Button, Message, Container, Header, Main, Alert, Card, Select, Option,
+  Popover, Divider, RadioGroup, Radio, Input, ColorPicker
+} from 'element-ui' */
+import { Button } from 'element-ui'
+// 国际化配置
+import enLang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+locale.use(enLang)
+// 将组建添加到 Vue 实例中
+Vue.use(Button)
