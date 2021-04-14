1ea2a9158c57d32979649eb9b9e499e7336750b5
strangersj <strangersj@outlook.com>
2021-04-14 21:16:04 +0800

    引入菜单栏组件

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,7 +4,11 @@ import Vue from 'vue'
   Button, Message, Container, Header, Main, Alert, Card, Select, Option,
   Popover, Divider, RadioGroup, Radio, Input, ColorPicker
 } from 'element-ui' */
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button, Form, FormItem, Input, Message,
+  Container, Header, Aside, Main,
+  Menu, Submenu, MenuItem
+} from 'element-ui'
 // 国际化配置
 import enLang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
@@ -14,5 +18,12 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 // 将 Message 挂载到 Vue 实例上
 Vue.prototype.$message = Message
