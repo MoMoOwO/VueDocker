169f1a1e8bafbc1205b603b32ed5d5838ab5ef52
strangersj <strangersj@outlook.com>
2021-06-03 21:34:18 +0800

    组件引入

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -9,12 +9,10 @@ import {
   Container, Header, Aside, Main,
   Menu, Submenu, MenuItem,
   Breadcrumb, BreadcrumbItem,
-  Card, Table, TableColumn
+  Card, Table, TableColumn,
+  Row, Col,
+  Pagination
 } from 'element-ui'
-// 国际化配置
-import enLang from 'element-ui/lib/locale/lang/en'
-import locale from 'element-ui/lib/locale'
-locale.use(enLang)
 // 将组建添加到 Vue 实例中
 Vue.use(Button)
 Vue.use(Form)
@@ -32,5 +30,8 @@ Vue.use(BreadcrumbItem)
 Vue.use(Card)
 Vue.use(Table)
 Vue.use(TableColumn)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Pagination)
 // 将 Message 挂载到 Vue 实例上
 Vue.prototype.$message = Message
