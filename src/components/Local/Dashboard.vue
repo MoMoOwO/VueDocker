04a82a3bfb31bf3fc55765cdda433d6496c8aab5
strangersj <strangersj@outlook.com>
2021-06-03 21:33:26 +0800

    bug fixed

--- src/components/Local/Dashboard.vue
+++ src/components/Local/Dashboard.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
+    <!-- 面包屑导航区域 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>主机信息</el-breadcrumb-item>
       <el-breadcrumb-item>概览</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card class="box-card">
-      <el-table :data="msg" style="width: 100%">
-        <el-table-column
-          align="center"
-          prop="HostName"
-          label="主机名称"
-          width="150"
-        >
+      <!-- 列表区域 -->
+      <el-table :data="HostInfoList" style="width: 100%" border stripe>
+        <el-table-column align="center" prop="HostName" label="主机名称">
         </el-table-column>
         <el-table-column align="center" label="CPU 信息">
           <el-table-column
@@ -45,7 +42,7 @@ export default {
   props: [],
   data() {
     return {
-      msg: []
+      HostInfoList: []
     }
   },
   created() {
@@ -56,7 +53,7 @@ export default {
     async getInfo() {
       const { data: res } = await this.axios.get('info')
       if (res.Code === 0) {
-        this.msg.push(res.Data)
+        this.HostInfoList.push(res.Data)
       } else {
         this.$message.error('获取主机信息失败！')
       }
